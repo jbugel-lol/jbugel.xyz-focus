5055eff3cb8c868b8e1cfa58afe86d22729e3353
Jonas Bugelnig <115107409+jbugel-lol@users.noreply.github.com>
2023-08-24 21:16:26 +0200

    Optimised

--- scripts/antiDistraction.js
+++
@@ -1,5 +0,0 @@
-// disable right-click on entire page
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-
-// disable text selection on entire page
-document.addEventListener("selectstart", (event) => event.preventDefault());
