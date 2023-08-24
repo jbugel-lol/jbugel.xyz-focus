30adb721d36c9f3da4ad581f8935afedc9e9aece
Jonas Bugelnig <115107409+jbugel-lol@users.noreply.github.com>
2023-08-24 21:18:51 +0200

    Add files via upload

---
+++ scripts/antiDistraction.js
@@ -0,0 +1,5 @@
+// disable right-click on entire page
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+// disable text selection on entire page
+document.addEventListener("selectstart", (event) => event.preventDefault());
